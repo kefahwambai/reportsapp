a9ed63de6d8be4cb3afb0a77aef2b125d3098bcf
KefahWambai <kefah.ngwei@student.moringaschool.com>
2023-11-08 10:22:25 +0300

    csrf error handling

--- src/Components/Authentication/csfr.js
+++ src/Components/Authentication/csfr.js
@@ -1,19 +1,21 @@
 const csrfFetch = async (url, options) => {
-  
   const jwtToken = localStorage.getItem('jwtToken');
 
-  
   const headers = {
     ...options.headers,
     'Content-Type': 'application/json',
     'Authorization': `Bearer ${jwtToken}`,
   };
 
-  
   const response = await fetch(url, { ...options, headers });
 
-  if (response.status >= 400) throw response;
-  
+  if (!response.ok) {
+    const errorData = await response.json().catch(() => ({})); // Handle non-JSON responses
+    const errorMessage = errorData.message || 'Request failed with no error message.';
+    
+    throw new Error(errorMessage);
+  }
+
   return response;
 };
 
